f2e03c169eec45abbe000e3b0608869e27ef7e3e
libisha <libisha01@gmail.com>
2026-03-04 12:48:15 +0530

    Add update and delete endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from typing import Any
 from random import randint
 
@@ -46,3 +46,25 @@ async def create_campaign(body: dict[str, Any]):
     
     data.append(new)
     return {"campaign" : new}
+
+@app.put("/campaigns/{id}")
+async def update_campaign(id: int, body: dict[str, Any]):
+    for index, campaign in enumerate(data):
+        if campaign.get("campaign_id") == id:
+            updated : Any = {
+                "campaign_id" : id,
+                "campaign_name" : body.get("name"),
+                "due_date" : body.get("due_date"),
+                "created_at" : campaign.get("created_at")
+            }
+            data[index] = updated
+            return {"campaign" : updated}
+    raise HTTPException(status_code=404)
+
+@app.delete("/campaigns/{id}")
+async def delete_campaign(id: int):
+    for index, campaign in enumerate(data):
+        if campaign.get("campaign_id") == id:
+            data.pop(index)
+            return Response(status_code=204)
+    raise HTTPException(status_code=404)
