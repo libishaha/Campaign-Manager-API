18296f1580ec63d3c25d6d6eebdc80d1fa6009a6
libisha <libisha01@gmail.com>
2026-03-12 17:28:04 +0530

    Set up sqlite database engine and table creation

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from datetime import datetime
 from fastapi import FastAPI, HTTPException, Response
 from typing import Any
 from random import randint
+from sqlmodel import create_engine, SQLModel
+
+sqlite_file_name ="database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread" : False}
+engine = create_engine(sqlite_url, connect_args = connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
 app = FastAPI(root_path="/api/v1")
 
